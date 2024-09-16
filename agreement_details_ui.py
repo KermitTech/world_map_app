a6476dabc2475b4904e642fd0eca75dae1a8904e
Atousa Ghanbari <atousa.ghanbari@kermittech.no>
2024-09-16 14:08:57 +0200

    Added a top button and title design to the page.

--- agreement_details_ui.py
+++ agreement_details_ui.py
@@ -5,8 +5,9 @@ from shiny import ui
 
 def agreement_details(agreement):
 
-    ui_details = ui.page_fluid(
+    ui_details = ui.div(ui.page_fluid(
         ui.input_action_button("show_country_details_page", "Agreement page", class_= 'country-details-btn'),
-        ui.h2(f"agreement details page for {agreement}"))
+        ui.h2(f"agreement details page for {agreement}", class_="agreement-details-title")),
+    class_="country-details-container")
 
     return ui_details 
