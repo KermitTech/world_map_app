d419d866a36af1ee0c9b768ad07e0a57acb2b05b
Atousa Ghanbari <atousa.ghanbari@kermittech.no>
2024-09-16 14:09:52 +0200

    Added a style container for the button groups.

--- country_details_ui.py
+++ country_details_ui.py
@@ -12,19 +12,14 @@ def country_details(country, df):
     pa_name = df[df['ISO3']==country]['pa_name']
     short_country_name = converter.convert(names=country, src="ISO3", to="name_short")
     
-    
     ui_details = ui.div(ui.page_fluid(
         ui.input_action_button("show_map_page", "Map Page", class_= 'country-details-btn'),
         # ui.h2(f"Welcome to the country details Page for {short_country_name}!", class_="country-details-title"), 
         ui.h2(f"{short_country_name}", class_="country-details-title"), 
-        *[ui.div(ui.input_action_button(f"{d}", pa_name.iloc[d], class_="agreement_btn")) for d in range(len(pa_name))],
-       
-        # ui.layout_columns(
-        #     *[ui.column(4,ui.div(ui.input_action_button(f"{d}", pa_name.iloc[d]), class_="agreement_btn")) for d in range(len(pa_name))],
-        #     # ui.column(8, ui.output_data_frame("table_agreement")), 
-        #     )
-
-
+        ui.div(
+            *[ui.div(ui.input_action_button(f"{d}", pa_name.iloc[d], class_="agreement_btn")) for d in range(len(pa_name))],
+            class_="agreement-container"
+        )
     ) , class_="country-details-container")
 
     return ui_details 
