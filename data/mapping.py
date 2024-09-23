d53bb7c7cd3b1965d6abe0de7d536c1f0b3b6672
Atousa Ghanbari <atousa.ghanbari@kermittech.no>
2024-09-23 13:57:06 +0200

    Initial commit.

---
+++ data/mapping.py
@@ -0,0 +1,34 @@
+import country_converter as coco
+from data.data_cleaning import new_df_disarm 
+
+########## Some aggregations on the data
+### number of peace agreements per country
+converter = coco.CountryConverter()
+
+
+
+no_distinct_gwno = new_df_disarm['gwno'].nunique()
+# print(no_distinct_gwno)
+
+counts_pa_perCountry = new_df_disarm.groupby('gwno')['pa_name'].count()
+
+### converting counts_pa_perCountry (it is a series) into a dataframe
+counts_pa_perCountry_df = counts_pa_perCountry.reset_index()
+
+### change the pa_column name
+counts_pa_perCountry_df = counts_pa_perCountry_df.rename(columns={"pa_name":"pa_counts"}) 
+
+#### convert the gwno code to iso3 and short country name
+iso3 = converter.convert(names=counts_pa_perCountry_df['gwno'], src="GWcode", to="ISO3")
+short_country_name = converter.convert(names=counts_pa_perCountry_df['gwno'], src="GWcode", to="name_short")
+
+#### insert the new columns into the dataframe counts_pa_perCountry_df
+counts_pa_perCountry_df.insert(loc=1, column='ISO3', value = iso3)
+counts_pa_perCountry_df.insert(loc=2, column='country_name', value = short_country_name)
+
+#### removed south Yemen
+counts_pa_perCountry_df = counts_pa_perCountry_df.drop(counts_pa_perCountry_df[counts_pa_perCountry_df['gwno'] == '678'].index)
+# print(counts_pa_perCountry_df)
+
+mapping  = dict(zip(counts_pa_perCountry_df['ISO3'].str.strip(), counts_pa_perCountry_df['pa_counts']))
+# print(mapping)
